021e9196def8355fa4fd056af26332cc3130c714
T-Search-Global <talha@searchglobal.org>
2025-07-08 01:44:00 +0500

    Update 2025_07_07_183450_add_date_to_events_table.php

--- database/migrations/2025_07_07_183450_add_date_to_events_table.php
+++ database/migrations/2025_07_07_183450_add_date_to_events_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
+            
          $table->date('date')->after('image')->nullable();
         });
     }

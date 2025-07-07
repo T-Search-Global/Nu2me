286575401a8d5b1c4ebd7aba51a93cb7181bad1c
T-Search-Global <talha@searchglobal.org>
2025-07-08 04:44:38 +0500

    pin response change!

--- app/Services/Pin/PinService.php
+++ app/Services/Pin/PinService.php
@@ -30,9 +30,37 @@ class PinService
 
     public function show()
     {
-        $pin = CreatePinModel::with('user')
+        $pins = CreatePinModel::with('user')
             ->select(['id', 'name', 'description', 'category', 'location', 'user_id', 'latitude', 'longitude'])
-            ->get();
-        return $pin;
+            ->get()
+            ->map(function ($pin) {
+                return [
+                    'id' => $pin->id,
+                    'title' => $pin->name, // renamed here
+                    'description' => $pin->description,
+                    'category' => $pin->category,
+                    'location' => $pin->location,
+                    'user_id' => $pin->user_id,
+                    'latitude' => (float) $pin->latitude,
+                    'longitude' => (float) $pin->longitude,
+
+                    'user' => [
+                        'id' => $pin->user->id ?? null,
+                        'first_name' => $pin->user->first_name ?? null,
+                        'last_name' => $pin->user->last_name ?? null,
+                        'email' => $pin->user->email ?? null,
+                        'email_verified_at' => $pin->user->email_verified_at ?? null,
+                        'phone' => $pin->user->phone ?? null,
+                        'city' => $pin->user->city ?? null,
+                        'country' => $pin->user->country ?? null,
+                        'img' => $pin->user->img ?? null,
+                        'deleted_at' => $pin->user->deleted_at ?? null,
+                        'is_paid' => $pin->user->is_paid ?? null,
+                        'img_url' => $pin->user->img_url ?? null,
+                    ]
+                ];
+            });
+
+        return $pins;
     }
 }

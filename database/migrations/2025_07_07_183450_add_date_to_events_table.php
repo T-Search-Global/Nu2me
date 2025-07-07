9983fa2bcef4c26062de933a11573ffa4f74ebb9
T-Search-Global <talha@searchglobal.org>
2025-07-08 01:41:16 +0500

    migattion date!

--- database/migrations/2025_07_07_183450_add_date_to_events_table.php
+++ database/migrations/2025_07_07_183450_add_date_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-      $table->dateTime('date')->nullable()->after('image');
+         $table->date('date')->after('image')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('events', function (Blueprint $table) {
-             $table->dropColumn('date');
+           $table->dropColumn('date');
         });
     }
 };
